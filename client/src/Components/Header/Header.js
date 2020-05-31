1e5dc753b13a8898b1082797f7b9ce8929f95be1
TylerKonesky <tylerkonesky@hotmail.com>
2020-05-31 12:18:56 -0600

    added git to client

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -10,9 +10,9 @@ class Header extends Component{
                 <div className="nav-wrapper header-color header-wrapper">
                     <a href="/" className="left brand-logo logo header-text">Taylorsville Football</a>
                     <ul className="right">
-                        <li><Link>Coaches</Link></li>
-                        <li><Link>Schedule</Link></li>
-                        <li><Link>Live</Link></li>
+                        <li><Link to="/coaches">Coaches</Link></li>
+                        <li><Link to="/schedule">Schedule</Link></li>
+                        <li><Link to="live">Live</Link></li>
                     </ul>
                 </div>
             </nav>
