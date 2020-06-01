f52e6ce68dbd6e4b6d593e150f21b09c81c0c33d
TylerKonesky <tylerkonesky@hotmail.com>
2020-06-01 15:23:29 -0600

    setup home page

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import M from 'materialize-css';
 import './Header.css'
 
 
 class Header extends Component{
+    componentDidMount(){
+        let elements = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elements)
+    }
     render(){
         return(
-            <nav>
-                <div className="nav-wrapper header-color header-wrapper">
-                    <a href="/" className="left brand-logo logo header-text">Taylorsville Football</a>
-                    <ul className="right">
-                        <li><Link to="/coaches">Coaches</Link></li>
-                        <li><Link to="/schedule">Schedule</Link></li>
-                        <li><Link to="live">Live</Link></li>
-                    </ul>
-                </div>
-            </nav>
+            <div>
+                <nav className="full-screen">
+                    <div className="nav-wrapper header-color header-wrapper">
+                        <a href="/" className="left brand-logo logo header-text">Taylorsville Football</a>
+                        <ul className="right">
+                            <li><Link to="/coaches">Coaches</Link></li>
+                            <li><Link to="/schedule">Schedule</Link></li>
+                            <li><Link to="live">Live</Link></li>
+                        </ul>
+                    
+                    </div>
+                </nav>
+                <nav className="mobile-screen"> 
+                    <div className="nav-wrapper header-color">   
+                        <a href="/" className="left brand-logo logo header-text">Taylorsville Football</a>
+                        <a className="dropdown-button right dropdown-button-settings dropdown-trigger" href="#!" data-target="dropdown"><i className="material-icons large">dehaze</i></a>
+                            <ul id="dropdown" className="dropdown-content"> 
+                                <li><Link to="/coaches">Coaches</Link></li>
+                                <li><Link to="/schedule">Schedule</Link></li>
+                                <li><Link to="live">Live</Link></li>
+                            </ul> 
+                    </div>
+                </nav>
+                
+
+            </div>
+            
+               
+           
         )
     }
 }
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
+import M from 'materialize-css';
+import './Home.css'
+import image1 from '../../images/image1.jpg'
+import logo from '../../images/logo.png'
 
 class Home extends Component{
+    componentDidMount(){
+        let elements = document.querySelectorAll('.parallax');
+        M.Parallax.init(elements)
+    }
     render(){
         return(
-            <div className="container">
-                This is the home page...
+            <div className="home-page-wrapper">
+                <div className="parallax-container">
+                    <div className="parallax parallax-images">
+                        <img src={image1} alt="Taylorsville Football Field"></img>
+                    </div>
+                </div>
+                <div className="section home-body-wrapper">
+                    <div className="row container">
+                        <div className="logo-wrapper">
+                            <img className="" src={logo} alt="Taylorsville Logo"></img>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="parallax-container">
+                    <div className="parallax parallax-images">
+                        <img className=""  src={image1} alt="Taylorsville Football Field"></img>
+                    </div>
+                </div>
             </div>
         )
     }
